34d28e270a4b5c994ba3eb81a301407eb0e3b6e4
W Martins <welingtonwtnmts@gmail.com>
2024-03-05 16:36:42 -0300

    Começando a implementar que imprime o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,7 +7,10 @@ namespace xadrez_console {
 
             Tabuleiro tab = new Tabuleiro(8, 8);
 
+            Tela.imprimirTabuleiro(tab);
+
             Console.ReadLine();
+
         }
     }
 }
--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -13,5 +13,9 @@ namespace tabuleiro {
             pecas = new Peca[linhas, colunas];
 
         }
+
+        public Peca peca(int linha, int coluna) {
+            return pecas[linha, coluna];
+        }
     }
 }
